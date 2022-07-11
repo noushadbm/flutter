71bb9b97974f3c547c8f8a20740c7708f88383c7
Abdul Noushad <AN00782897@techmahindra.com>
2022-07-11 22:06:30 +0530

    first commit

---
+++ github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'e4d5610b962989c1db4c';
+const githubClientSecret = '349a907f6f9633d19b3c1d7ad6740754cd99f0be';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
